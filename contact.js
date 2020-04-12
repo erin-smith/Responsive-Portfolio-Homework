fdc94d075641319fd844bb0acff80511adfada8e
Erin Smith <erin.acumen@gmail.com>
2020-04-11 18:31:05 -0700

    used html validatior

--- contact.js
+++ contact.js
@@ -1,9 +1,5 @@
 $(function () {
 
-    // init the validator
-    // validator files are included in the download package
-    // otherwise download from http://1000hz.github.io/bootstrap-validator
-
     $('#contact-form').validator();
 
 
